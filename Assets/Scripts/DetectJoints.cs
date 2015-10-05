e8c2fd2252e104c6d1d8a0964d5e078a71714078
zdwchang <zdwchang@gmail.com>
2015-10-05 11:25:58 +0800

    detect joints

---
+++ Assets/Scripts/DetectJoints.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class DetectJoints : MonoBehaviour {
+
+
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    private Body[] bodies;
+
+	// Use this for initialization
+	void Start () {
+	    if (BodySrcManager == null)
+        {
+            Debug.Log("Assign Game Object with Body Source Manager");
+        }
+        else
+        {
+            bodyManager = BodySrcManager.GetComponent<BodySourceManager>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (bodyManager == null)
+        {
+            return;
+        }
+        else
+        bodies = bodyManager.GetData();
+
+        if (bodies == null)
+        {
+            return;
+        }
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                var pos = body.Joints[TrackedJoint].Position;
+                gameObject.transform.position = new Vector3(pos.X, pos.Y);
+            }
+        }
+	}
+}
